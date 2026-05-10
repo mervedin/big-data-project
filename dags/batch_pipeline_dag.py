03b1c1ff3d8c1234a4c1fe19b4ad92436e0e3004
Merve Din <mervedin@172-2-2-33.lightspeed.dybhfl.sbcglobal.net>
2026-05-10 23:14:31 +0300

    fixed airflow issues

--- dags/batch_pipeline_dag.py
+++ dags/batch_pipeline_dag.py
@@ -6,19 +6,29 @@ from datetime import datetime
 with DAG(
     dag_id="batch_kafka_spark_pipeline",
     start_date=datetime(2024, 1, 1),
-    schedule=None,          # manual trigger (recommended for course)
+    schedule=None,
     catchup=False,
     tags=["big-data", "batch"],
 ) as dag:
 
     produce_to_kafka = BashOperator(
         task_id="produce_to_kafka",
-        bash_command="docker-compose run --rm kafka-producer",
+        bash_command="""
+        docker run --rm \
+          --network big-data-project_default \
+          -v /Users/mervedin/Desktop/big_data/big-data-project/data:/data \
+          big-data-project-kafka-producer:latest
+        """,
     )
 
     run_spark_job = BashOperator(
         task_id="run_spark_job",
-        bash_command="docker-compose run --rm spark-job",
+        bash_command="""
+        docker run --rm \
+          --network big-data-project_default \
+          -v /Users/mervedin/Desktop/big_data/big-data-project/results:/results \
+          big-data-project-spark-job:latest
+        """,
     )
 
     produce_to_kafka >> run_spark_job
